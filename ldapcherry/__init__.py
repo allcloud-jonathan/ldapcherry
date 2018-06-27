8344343312bc1c75d64751c0a458b91e2ac40415
Jonathan Vogt <jonathan.vogt@allcloud.io>
2018-06-27 16:54:57 +0200

    Revert "catch password error"
    This reverts commit d6cee4de325327167440e4adc958e08cea6ed979.

--- ldapcherry/__init__.py
+++ ldapcherry/__init__.py
@@ -735,10 +735,6 @@ class LdapCherry(object):
                 self._add_notification(
                     'User does not exist in backend "' + b + '"'
                     )
-            except PPolicyError:
-                self._add_notification(
-                    'Password does not match policy'
-                    )
 
         return badd
 
